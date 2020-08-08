6f1a3dbca93ef4cb5cea9acdb135715f1563e473
AlanGuo <AlanGuo>
2020-08-08 22:23:43 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+var LambdaWatermark = require("./lib/lambdaWatermark");
+
+var options = {
+  srcPath: "image/",
+  destPath: "media/image/",
+  distBucket: "newonlyfans-public",
+  getDistKey: (srcKey, opt) => {
+    return opt.destPath + srcKey.replace(opt.srcPath, "")
+  },
+  watermarkImagePath: "./bitapp-white.png",
+  opacity: 100
+};
+
+exports.handler = function(event, context) {
+  new LambdaWatermark(options)(event, context);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('WaterMark added!'),
+  };
+  return response;
+};
